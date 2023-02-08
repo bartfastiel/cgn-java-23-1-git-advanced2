17069607a81bfcbd5e17bb2496909993d1664c58
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-02-08 11:56:08 +0100

    !2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("second line");
         System.out.println("wähh!");
-        System.out.println("Haha");
+        System.out.println("Haha!");
         System.out.println("Hihi");
         System.out.println("Ich habe tolle Farben gemacht!");
     }
